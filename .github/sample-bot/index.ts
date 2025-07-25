0df72f23d1ae4ee718363dc336c34a2ec4e3ff8e
cobalt <61329810+cobaltt7@users.noreply.github.com>
2025-07-24 20:41:13 -0500

    Test strife and sample together
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

--- .github/sample-bot/index.ts
+++
@@ -1,9 +0,0 @@
-import { fileURLToPath } from "node:url";
-
-import { GatewayIntentBits } from "discord.js";
-import { login } from "strife.js";
-
-await login({
-	modulesDirectory: fileURLToPath(new URL("./modules", import.meta.url)),
-	clientOptions: { intents: GatewayIntentBits.Guilds },
-});
--- .github/sample-bot/modules/ping.ts
+++
@@ -1,5 +0,0 @@
-import { defineChatCommand } from "strife.js";
-
-defineChatCommand({ name: "ping", description: "Ping!" }, async (interaction) => {
-	await interaction.reply("Pong!");
-});
