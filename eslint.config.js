7ca7eec2accf036795386578320c6ed94a8a25dd
cobalt <61329810+cobaltt7@users.noreply.github.com>
2026-05-20 17:06:35 -0500

    bump config
    Signed-off-by: cobalt <61329810+cobaltt7@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
-import cobaltConfigs, { declareConfig, globals } from "eslint-config-cobaltt7";
-
-export default declareConfig({ files: ["**/*.ts"] }, { ignores: ["dist"] }, ...cobaltConfigs, {
+import cobaltConfigs, { globals } from "eslint-config-cobaltt7";
+import { defineConfig, globalIgnores } from "eslint/config";
+export default defineConfig(
+	{ files: ["**/*.ts"] },
+	globalIgnores(["dist"]),
+	...cobaltConfigs,{
 	languageOptions: {
 		ecmaVersion: 2022,
 		globals: globals.nodeBuiltin,
